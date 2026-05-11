096222279e03ab2c5a737fb26549fb9257f74770
Bodlev Laurentiu <lbodlev888@gmail.com>
2026-05-11 09:27:19 +0300

    added dedicated functions for parsing pq keys in crypto module of the project

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -37,3 +37,21 @@ func GetPublicKey(pubKey string) {
 
 	fmt.Printf("Public: %s\n", base64.StdEncoding.EncodeToString(encaps))
 }
+
+func ParseDecapsKey(decaps_str string) (*mlkem.DecapsulationKey768, error) {
+	raw_decaps, err := base64.StdEncoding.DecodeString(decaps_str)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode private key: %w", err)
+	}
+
+	return mlkem.NewDecapsulationKey768(raw_decaps)
+}
+
+func ParseEncapsKey(encaps_str string) (*mlkem.EncapsulationKey768, error) {
+	raw_encaps, err := base64.StdEncoding.DecodeString(encaps_str)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode public key: %w", err)
+	}
+
+	return mlkem.NewEncapsulationKey768(raw_encaps)
+}
