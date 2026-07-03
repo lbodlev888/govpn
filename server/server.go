66ac0e8155860f921693fddfb8ed2d14fbd5a7a3
Bodlev Laurentiu <lbodlev888@gmail.com>
2026-07-03 23:46:27 +0300

    fix: validation on newpeer

--- server/server.go
+++ server/server.go
@@ -67,7 +67,6 @@ func Run(ctx context.Context) {
 
 	wg.Go(func() { readFromPeers(ctx) })
 	wg.Go(func() { readFromIface(ctx) })
-	wg.Go(func() { listenForSignals(ctx) }) //to be removed in the future
 
 	wg.Go(func() {
 		<-ctx.Done()
@@ -78,11 +77,17 @@ func Run(ctx context.Context) {
 	wg.Wait()
 }
 
-func AddNewPeer(peer config.PeerConfig) {
+func NewPeer(peer config.PeerConfig) error {
 	allowedPeersMu.Lock()
 	defer allowedPeersMu.Unlock()
 
+	if err := checkEncapsulation(peer.EncapsKey); err != nil {
+		return fmt.Errorf("NewPeer: invalid encapsulation key: %w", err)
+	}
+
 	allowedPeers[peer.Name] = peer
+
+	return nil
 }
 
 func GetAllPeers() []config.PeerConfig {
--- server/utils.go
+++ server/utils.go
@@ -1,11 +1,9 @@
 package server
 
 import (
-	"context"
-	"log"
-	"net"
-	"os"
-	"strings"
+	"crypto/mlkem"
+	"encoding/base64"
+	"fmt"
 )
 
 func loadAllowedPeers() {
@@ -19,57 +17,12 @@ func loadAllowedPeers() {
 	}
 }
 
-func listenForSignals(ctx context.Context) {
-	os.Remove("/tmp/ownvpn.sock")
-	unixListener, err := net.Listen("unix", "/tmp/ownvpn.sock")
+func checkEncapsulation(encaps string) error {
+	rawEncaps, err := base64.StdEncoding.DecodeString(encaps)
 	if err != nil {
-		log.Println("failed to aquire listener: " + err.Error())
-		return
-	}
-	defer unixListener.Close()
-
-	wg.Go(func() {
-		<-ctx.Done()
-		unixListener.Close()
-	})
-
-	for {
-		client, err := unixListener.Accept()
-		if err != nil {
-			if ctx.Err() != nil {
-				return
-			}
-
-			log.Println("Failed to accept client: " + err.Error())
-			continue
-		}
-
-		wg.Go(func() {
-			handleUnixClient(client)
-		})
-	}
-}
-
-func handleUnixClient(client net.Conn) {
-	client.Write([]byte("Input command: "))
-	buf := make([]byte, 1024)
-	n, err := client.Read(buf)
-	if err != nil {
-		log.Println("Failed to read command from unix client: " + err.Error())
-		return
+		return fmt.Errorf("checkEncapsulation: base64decode: %w", err)
 	}
 
-	input := strings.TrimSpace(string(buf[:n]))
-	command := strings.Split(input, ":")
-	switch command[0] {
-	case "enable":
-		EnablePeer(command[1])
-	case "disable":
-		DisablePeer(command[1])
-	case "remove":
-		RemovePeer(command[1])
-	case "export":
-		ExportPeerSettings(command[1])
-	}
-	client.Write([]byte("Executed"))
+	_, err = mlkem.NewEncapsulationKey768(rawEncaps)
+	return err
 }
